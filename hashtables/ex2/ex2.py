552fb818b6fd170a5401483ff8022446dc2ac0d4
Jeremy Morales <jeremy.morales@gmail.com>
2020-07-19 19:43:23 -0700

    ex2 tests pass

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,29 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    ticket_dict = dict()
+
+    route = []
+    for ticket in tickets:
+        ticket_dict[ticket.source] = ticket.destination
+    current_dest = ticket_dict["NONE"]
+    pos = 0
+    while pos < length:
+        route.append(current_dest)
+        current_dest = ticket_dict[current_dest]
+        pos +=1
+
+
+
+
 
     return route
+
+
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
+
+# tickets = [ticket_1, ticket_2, ticket_3]
+
+# print(reconstruct_trip(tickets, 3))
