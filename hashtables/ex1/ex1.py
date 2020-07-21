84732e8daa3138a4d0d0e38b3bd685bd5bba4b1e
Jeremy Morales <jeremy.morales@gmail.com>
2020-07-20 19:09:14 -0700

    ex1 tests pass

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,49 +3,31 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
-    # weight / list position for k, v in dict
     weight_dict = dict()
-
-    i = 0
-    # while i < len(weights):
-    #     if weights[i] <= limit:
-    #         if weight_dict[weights[i]] is not None:
-    #             print("already present")
-    #         else:
-    #             print("adding to dict list")
-    #             # weight_dict[weights[i]] = {i}
-    #     i += 1
-    # print(weight_dict)
-    result = ()
-    while i < len(weights):
-        check_weight = weights[i]
-        if check_weight <= limit:
-
-            if check_weight in weight_dict and target / check_weight == 2:
-                if target / weights[i] == 2:
-                    result = (weight_dict[weights[i]])
-
-            else:
-                weight_dict[weights[i]] = i
-        i += 1
-
-    print(weight_dict)
+    pos = 0
+    for i in weights:
+        if i in weight_dict:
+            weight_dict[i] = [weight_dict[i], pos]
+        else:
+            weight_dict[i] = pos
+        pos += 1
 
     for key in weight_dict:
         target = limit - key
 
-        print(target)
         if target in weight_dict:
 
-            if target > key:
-                result = (weight_dict[target], weight_dict[key])
-            result = (int(weight_dict[key]), int(weight_dict[target]))
+            if target > 0 and limit / target == 2:
+                a = weight_dict[target][1]
+                b = weight_dict[target][0]
 
-    return result
+            else:
+                a = weight_dict[target]
+                b = weight_dict[key]
+            return ([a, b])
 
 
-weights_3 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+weights_3 = [4, 4]
 
 
-print(get_indices_of_item_weights(weights_3, 9, 7))
+print(get_indices_of_item_weights(weights_3, 2, 8))
