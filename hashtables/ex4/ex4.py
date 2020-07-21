5595a1db4326f07361c5eaf33b9ab82a24a29c57
Jeremy Morales <jeremy.morales@gmail.com>
2020-07-20 18:46:34 -0700

    ex4 completes succesfully

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,26 +1,55 @@
 # from hashtables.ex4.hashtable import HashTable, HashTableEntry, Bucket
-from hashtable import HashTable, HashTableEntry, LinkedList
-
-
-results = []
 
 
 def has_negatives(a):
     """
     YOUR CODE HERE
-    """
-    # Your code here
-    new_capacity = len(a)
-    num_table = HashTable(new_capacity)
-    print(num_table.capacity)
 
-    for item in a:
-        opposite = -item
-        # print(item)
-        num_table.put(str(item), 0)
-        if num_table.get(str(opposite)) is not None:
-            results.append(abs(item))
-    print(results)
+    tried using hashtable, not efficient enough, fails on large arrays
+
+    """
+
+    results = []
+
+    # Your code here
+    # new_capacity = len(a)
+    # num_table = HashTable(new_capacity)
+    # print(num_table.capacity)
+
+    # for item in a:
+    #     opposite = -item
+    #     # print(item)
+    #     num_table.put(str(item), 0)
+    #     if num_table.get(str(opposite)) is not None:
+    #         results.append(abs(item))
+    # print(results)
+    # return results
+
+    """
+    Below uses list comprehension completes ~ 0.9s
+    """
+    # positive = dict()
+    # negative = dict()
+
+    # for i in a:
+    #     if i > 0:
+    #         positive[i] = 0
+    #     if i < 0:
+    #         negative[abs(i)] = 0
+
+    # results = [key for key in positive if key in negative]
+
+    """
+    variation 2 test completes ~0.95s
+    """
+    data = dict.fromkeys(a, "")
+
+    for i in a:
+        opposite = -i
+        if i > 0:
+            if opposite in data:
+                results.append(i)
+
     return results
 
 
